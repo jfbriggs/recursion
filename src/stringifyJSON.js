548a894b5985750b620c567138be3e16eec136c6
James Briggs <jfbriggs106@gmail.com>
2016-11-27 22:32:18 -0800

    Create initial base cases for non-object/array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
+// STEP 1: Figure out what type of element you're dealing with to begin with
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === undefined || typeof obj === 'function') {
+    return undefined;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return "" + obj;
+  }
 };
+
+
+// {a: 1, b: 2, c: {z: 1, y: 3}, d: 4}
+
+// ==> "{"a":1,"b":2,"c":{"z":1,"y":3},"d":4}"
+
+var toString = function(element) {
+  if (typeof element === 'string' || typeof element === 'object' || typeof element === 'boolean' || typeof element === 'number') {
+    return "" + element + "";
+  }
+}
+
+
+function stringify(thing) {
+  var result;
+  if Array.isArray(thing) {
+    var result = ["["];
+    run function that pushes all remaining elements to result;
+    result.push("]");
+    return result.join(",");
+  }
+}
+
+// ['a', 'b', 'c', ['a', 'b', 3], 'd']
+['[', '"a"', '"b"', '"c"', "[3,1,3]", '"d"', ']']
+
